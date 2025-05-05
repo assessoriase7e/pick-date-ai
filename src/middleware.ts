c2dcfeea791167e885d914a5eb73e5be9fa5f369
assessoriase7e <se7eestudiodedesign@gmail.com>
2025-05-05 16:33:28 -0300

    update: clerk auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,18 +3,18 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/api/api-keys(.*)",
-  // Não proteger a rota de calendário compartilhado
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId, redirectToSignIn } = await auth();
-  if (!userId && isProtectedRoute(req)) {
-    return redirectToSignIn();
-  }
-
   // Permitir acesso à rota de calendário compartilhado sem autenticação
   if (req.nextUrl.pathname.startsWith("/shared-calendar")) {
     return;
+  }
+
+  const { userId, redirectToSignIn } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    return redirectToSignIn();
   }
 });
 
