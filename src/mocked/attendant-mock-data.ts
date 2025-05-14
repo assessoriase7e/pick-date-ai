c33967583a5499dc987f498331a1d4b7a59446c5
assessoriase7e <se7eestudiodedesign@gmail.com>
2025-05-14 13:49:01 -0300

    update: mock data

--- src/mocked/attendant-mock-data.ts
+++ src/mocked/attendant-mock-data.ts
@@ -22,6 +22,9 @@ export const defaultRules = [
     rule: "Consulte a base de conhecimento para verificar o período de funcionamento do estabelecimento.",
   },
   {
+    rule: "Não fale sobre horários de outros clientes.",
+  },
+  {
     rule: "Na primeira interação, verifique se o cliente já está cadastrado através da tool getClientInfo. Caso contrário, informe que é necessário o cadastro e se apresente como uma solução automatizada.",
   },
   {
