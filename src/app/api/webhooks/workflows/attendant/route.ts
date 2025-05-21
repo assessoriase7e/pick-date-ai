7c5ffbeed96ba797921e21d44aacea88bd9d9af1
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-05-20 23:32:38 -0300

    update: attendant route

--- src/app/api/webhooks/workflows/attendant/route.ts
+++ src/app/api/webhooks/workflows/attendant/route.ts
@@ -1,18 +1,31 @@
-import { prisma } from "@/lib/db";
 import { EvolutionData } from "@/types/evolutionData";
+import { checkBlocked } from "@/workflows/utils/checkBlocked";
+import { messageBuffer } from "@/workflows/utils/messageBuffer";
+import { setMessage } from "@/workflows/utils/setMessage";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-    const body = (await req.json()) as EvolutionData;
-    const number = body.data.key.remoteJid.split("@")[0];
+  const body = (await req.json()) as EvolutionData;
+  const number = body.data.key.remoteJid.split("@")[0];
 
-    const isBLocked = await prisma.blackListPhone.findFirst({
-        where: { number },
-    });
+  //Check blcked
+  const isBLocked = await checkBlocked(body.instance, number);
 
-    if (!!isBLocked) {
-        return NextResponse.json({ success: false }, { status: 200 });
-    }
+  if (!!isBLocked) {
+    return NextResponse.json({ success: false }, { status: 200 });
+  }
 
-    return NextResponse.json({ success: true }, { status: 200 });
+  const message = await setMessage({ evoData: body });
+
+  if (!message) {
+    return NextResponse.json({ success: false }, { status: 200 });
+  }
+
+  await messageBuffer({
+    remoteId: body.instance,
+    newMessage: message,
+    delayInSeconds: 7,
+  });
+
+  return NextResponse.json({ success: true }, { status: 200 });
 }
