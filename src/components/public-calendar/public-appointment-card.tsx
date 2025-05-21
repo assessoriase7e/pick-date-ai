23a466944f949cc65d6fcc5d5253117e273ff7ae
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-05-21 11:27:18 -0300

    update: public appointment card layout

--- src/components/public-calendar/public-appointment-card.tsx
+++ src/components/public-calendar/public-appointment-card.tsx
@@ -13,29 +13,19 @@ export function PublicAppointmentCard({
 }: PublicAppointmentCardProps) {
   return (
     <div
-      className="bg-primary p-2 flex flex-col justify-between cursor-pointer z-50"
+      className="bg-primary p-2 flex flex-col justify-center cursor-pointer rounded"
       style={{ height: `${height}px`, minHeight: "20px" }}
       onClick={(e) => {
         e.stopPropagation();
         onClick(appointment);
       }}
     >
-      <div className="flex items-center gap-2 text-sm text-muted-foreground"></div>
-      <div className="flex flex-col gap-1">
-        <span className="font-medium text-sm flex gap-2 items-center">
-          {appointment?.client?.fullName || "Horário Reservado"}
-          {appointment.service?.name && (
-            <span className="ml-2 text-xs text-muted-foreground line-clamp-1">
-              • {appointment.service.name}
-            </span>
-          )}
-        </span>
-        {appointment.notes && (
-          <span className="text-xs text-muted-foreground line-clamp-2">
-            {appointment.notes}
-          </span>
-        )}
-      </div>
+      <div className="flex items-center gap-2 text-sm "></div>
+      <h4 className="font-medium truncate flex items-center gap-2 text-background dark:text-foreground text-xs lg:text-md">
+        <p>{appointment.client?.fullName || "Cliente Deletado"}</p>{" "}
+        <span> | </span>
+        <p>{appointment.service.name}</p>{" "}
+      </h4>
     </div>
   );
 }
