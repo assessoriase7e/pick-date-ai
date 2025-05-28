19464566c56df87c8e3617b2cbe33d74c8a8cfef
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-05-28 01:51:28 -0300

    update: dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,5 @@
 import type React from "react";
 import { Sidebar } from "@/components/sidebar";
-import { Navbar } from "@/components/navbar";
 
 export default async function DashboardLayout({
   children,
@@ -11,7 +10,6 @@ export default async function DashboardLayout({
     <div className="flex min-h-screen dark:bg-gradient-to-t dark:from-zinc-900/25 to-background">
       <Sidebar />
       <div className="flex-1 flex flex-col">
-        <Navbar />
         <main className="flex-1 container p-5 py-5 mx-auto max-w-[1200px] ">
           {children}
         </main>
