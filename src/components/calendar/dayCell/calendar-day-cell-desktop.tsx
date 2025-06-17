b81343deb79a0532ea1eab08bb754fad4c86510e
Se7e <se7estudiodedesign@gmail.com>
2025-06-16 22:26:31 -0300

    update: calendar day cell desktop

--- src/components/calendar/dayCell/calendar-day-cell-desktop.tsx
+++ src/components/calendar/dayCell/calendar-day-cell-desktop.tsx
@@ -105,7 +105,7 @@ export function CalendarDayCellDesktop({
             {dayObj.date.date()}
           </span>
           {activeAppointments.length > 0 && (
-            <span className={appointmentCountStyles}>
+            <span className={cn(appointmentCountStyles, "flex justify-center items-center w-full")}>
               {activeAppointments.length}
               <span className="hidden lg:block ml-1">Clientes</span>
             </span>
