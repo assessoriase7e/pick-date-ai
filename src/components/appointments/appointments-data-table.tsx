cb1e1d2065c03aa274484e7a2ee65fa73ba55238
Se7e <se7estudiodedesign@gmail.com>
2025-07-04 22:44:10 -0300

    update: appointment data table -> send reminder button color

--- src/components/appointments/appointments-data-table.tsx
+++ src/components/appointments/appointments-data-table.tsx
@@ -247,7 +247,11 @@ export function AppointmentsDataTable({
             </Button>
           </>
         )}
-        <Button onClick={handleNotificationClick} className="flex items-center gap-2 w-full md:w-min" variant="outline">
+        <Button
+          onClick={handleNotificationClick}
+          className="flex items-center gap-2 w-full md:w-min text-primary"
+          variant="outline"
+        >
           <MessageCircle className="h-4 w-4" />
           Emitir aviso
         </Button>
