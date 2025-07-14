8415fc4dcf851d71b8e4b33ae25300bebd43795a
Se7e <se7estudiodedesign@gmail.com>
2025-07-14 15:36:19 -0300

    fix: main dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ export default async function DashboardLayout({ children }: { children: React.Re
       <div className="flex flex-col min-h-screen dark:bg-gradient-to-t dark:from-zinc-900/25 to-background">
         <DockMenu />
         <div className="flex-1 flex flex-col lg:pb-20">
-          <main className="flex-1 container p-5 py-5 mx-auto max-w-[1200px] ">{children}</main>
+          <main className="flex-1 container mx-auto max-w-[1200px] ">{children}</main>
         </div>
       </div>
     </ThemeProvider>
